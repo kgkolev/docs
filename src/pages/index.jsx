356daeb95d7f46bdf4f8eda27c42ef9936d676d0
Teck Liew <teck.liew@lge.com>
2017-12-14 13:23:48 -0800

    update siteConfig

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,61 +9,61 @@ import CtaButton from '../components/CtaButton'
 
 class Index extends React.Component {
 
-  render() {
-    const postEdges = this.props.data.allMarkdownRemark.edges;
-    return (
-      <div className="index-container">
-        <Helmet title={config.siteTitle} />
-        <SEO postEdges={postEdges} />
-        <main>
-          <MainHeader
-            siteTitle={config.siteTitle}
-            siteDescription={config.siteDescription}
-            location={this.props.location}
-            logo={config.siteLogo}
-          />
-          <BodyContainer>
-            <h2>A Gatsby Template for Content</h2>
-            <p>Made for modern documentation sites. Table of Contents automatically generated from markdown files. </p>
-            <CtaButton to={'/lesson-one'}>See Your First Post</CtaButton>
-          </BodyContainer>
-        </main>
-      </div>
-    );
-  }
+	render() {
+		const postEdges = this.props.data.allMarkdownRemark.edges;
+		return (
+			<div className="index-container">
+				<Helmet title={config.siteTitle} />
+				<SEO postEdges={postEdges} />
+				<main>
+					<MainHeader
+						siteTitle={config.siteTitle}
+						siteDescription={config.siteDescription}
+						location={this.props.location}
+						logo={config.siteLogo}
+					/>
+					<BodyContainer>
+						<h2>A Enact Template for Content</h2>
+						<p>Made for modern documentation sites. Table of Contents automatically generated from markdown files. </p>
+						<CtaButton to={'/lesson-one'}>See Your First Post</CtaButton>
+					</BodyContainer>
+				</main>
+			</div>
+		);
+	}
 }
 
 export default Index;
 
 const BodyContainer = styled.div`
-  padding: ${props => props.theme.sitePadding};
-  max-width: ${props => props.theme.contentWidthLaptop};
-  margin: 0 auto;
+	padding: ${props => props.theme.sitePadding};
+	max-width: ${props => props.theme.contentWidthLaptop};
+	margin: 0 auto;
 `
 
 
 /* eslint no-undef: "off"*/
 export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      limit: 2000
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      edges { 
-        node {
-          fields {
-            slug
-          }
-          excerpt
-          timeToRead
-          frontmatter {
-            title
-            tags
-            cover
-            date
-          }
-        }
-      }
-    }
-  }
+	query IndexQuery {
+		allMarkdownRemark(
+			limit: 2000
+			sort: { fields: [frontmatter___date], order: DESC }
+		) {
+			edges {
+				node {
+					fields {
+						slug
+					}
+					excerpt
+					timeToRead
+					frontmatter {
+						title
+						tags
+						cover
+						date
+					}
+				}
+			}
+		}
+	}
 `;
